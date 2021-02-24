cf55ac2cb114c783a80bf45d5b8d84c5be417f8e
kyoungkim <ralrudtjq01@gmail.com>
2021-02-24 14:18:42 +0900

    first

---
+++ catalog/admin.py
@@ -0,0 +1,64 @@
+from django.contrib import admin
+
+# Register your models here.
+from catalog.models import Author, Genre, Book, BookInstance, Language
+
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+# @admin.register(BookInstance)
+
+class BookInstance(admin.ModelAdmin):
+    list_filter = ('book', 'status', 'due_back', 'id')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+  #  = ['book', 'status', 'due_back', 'id']
+
+
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+# admin.site.register(Author)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+
+admin.site.register(Genre)
+
+admin.site.register(Language)
